f78f708660cd6261b9bfbf7d314525f5c9fdceb6
Adam <adam.reichner.049@k12.friscoisd.org>
2020-10-19 11:47:35 -0500

    Added map feature

---
+++ GitTutorial/MapForItinerary.swift
@@ -0,0 +1,21 @@
+//
+//  MapForItinerary.swift
+//  GitTutorial
+//
+//  Created by Reichner, Adam R on 10/19/20.
+//  Copyright © 2020 Reichner, Adam R. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapForItinerary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapForItinerary_Previews: PreviewProvider {
+    static var previews: some View {
+        MapForItinerary()
+    }
+}
